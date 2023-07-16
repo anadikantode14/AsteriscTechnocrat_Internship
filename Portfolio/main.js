10e1ad10febc74d688dbca5489820fa2e46ceeed
unknown <anadikantode@gmail.com>
2023-07-16 17:48:07 +0530

    Add folder1, folder2, folder3

---
+++ Portfolio/main.js
@@ -0,0 +1,51 @@
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const menu = document.querySelector('nav .container ul');
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = 'block';
+    menuBtn.style.display = 'none';
+    closeBtn.style.display = 'inline-block';
+})
+
+closeBtn.addEventListener('click', () => {
+    menu.style.display = 'none';
+    menuBtn.style.display = 'inline-block';
+    closeBtn.style.display = 'none';
+})
+
+const navItems = menu.querySelectorAll('li');
+
+const changeActiveItem = () => {
+    navItems.forEach(item => {
+        const link = item.querySelector('a');
+        link.classList.remove('active');
+    })
+}
+navItems.forEach(item => {
+    const link = item.querySelector('a');
+    link.addEventListener('click', () => {
+        changeActiveItem();
+        link.classList.add('active');
+    })
+})
+
+const readMoreBtn = document.querySelector('.read-more');
+const readMoreContent = document.querySelector('.read-more-content');
+
+readMoreBtn.addEventListener('click', () => {
+    readMoreContent.classList.toggle('show-content');
+    if(readMoreContent.classList.contains('show-content')){
+        readMoreBtn.textContent = "Show less";
+    }else {
+        readMoreBtn.textContent = "Show more";
+    }
+})
+
+const skillItems = document.querySelectorAll('section.skills .skill');
+
+skillItems.forEach(skill => {
+    skill.querySelector('.head').addEventListener('click', () => {
+        skill.querySelector('.items').classList.toggle('show-items');
+    });
+});
